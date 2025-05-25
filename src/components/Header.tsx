ea6085788b9d42641b83526b2c3ed884e0abd85a
devanshh-garg <devanshgarg.penn@gmail.com>
2025-05-26 01:52:34 +0530

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,7 +11,7 @@ const Header: React.FC = () => {
           to="/" 
           className="flex items-center gap-2 text-indigo-600 font-bold text-xl"
         >
-          <Croissant size={24} />
+          <Croissant size={32} />
           <span>WordDuel</span>
         </Link>
         
