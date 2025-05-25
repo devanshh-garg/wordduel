c436d42450c36c3fec3cd33a81c72bd7c10a8e06
devanshh-garg <devanshgarg.penn@gmail.com>
2025-05-26 01:59:43 +0530

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { BookIcon, Moon, Sun } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
 const Header: React.FC = () => {
-  const [isDarkMode, setIsDarkMode] = useState(() => localStorage.getItem('theme') === 'dark' || (window.matchMedia('(prefers-color-scheme: dark)').matches && localStorage.getItem('theme') !== 'light')));
+  const [isDarkMode, setIsDarkMode] = useState(() => localStorage.getItem('theme') === 'dark' || (window.matchMedia('(prefers-color-scheme: dark)').matches && localStorage.getItem('theme') !== 'light'))):
   return (
     <header className="w-full bg-white border-b border-slate-200 py-4 px-4">
       <div className="max-w-3xl mx-auto flex items-center justify-between">
