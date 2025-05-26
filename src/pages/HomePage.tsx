467a3b59641ebd10dfba89f204dc4331670e49fa
devanshh-garg <devanshgarg.penn@gmail.com>
2025-05-27 03:38:42 +0530

    Deploy application to Netlify

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import CreateChallenge from '../components/CreateChallenge';
-import Footer from '../components/Footer';
 
 const HomePage: React.FC = () => {
   return (
-    <div className="min-h-screen bg-slate-50 dark:bg-gray-900 flex flex-col">
+    <div className="min-h-screen bg-slate-50 dark:bg-gray-900">
       <Header />
       <main className="flex-grow">
         <CreateChallenge />
       </main>
-      <Footer />
     </div>
   );
 };
