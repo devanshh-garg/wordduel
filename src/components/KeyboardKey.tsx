d6063c1644fef93fa1e5f928dc68ec840bb3dab7
devanshh-garg <devanshgarg.penn@gmail.com>
2025-05-27 03:53:12 +0530

    Deploy application to Netlify

--- src/components/KeyboardKey.tsx
+++ src/components/KeyboardKey.tsx
@@ -9,10 +9,10 @@ interface KeyboardKeyProps {
 }
 
 const stateToColor = {
-  correct: 'bg-emerald-500',
+  correct: 'bg-emerald-600',
   present: 'bg-amber-500',
-  absent: 'bg-slate-600/90 dark:bg-gray-600/90',
-  unused: 'bg-slate-600/70 dark:bg-gray-600/70'
+  absent: 'bg-gray-600',
+  unused: 'bg-gray-600/80'
 };
 
 const KeyboardKey: React.FC<KeyboardKeyProps> = ({ letter = '', state = 'unused', onClick }) => {
@@ -29,14 +29,13 @@ const KeyboardKey: React.FC<KeyboardKeyProps> = ({ letter = '', state = 'unused'
     <motion.button
       whileTap={{ scale: 0.95 }}
       className={`
-        min-w-[38px] h-[52px] text-lg font-medium
+        min-w-[32px] h-[45px] text-base font-medium
         flex items-center justify-center
-        rounded-lg
+        rounded-md
         transition-colors duration-200
         ${stateToColor[state]}
         text-white
-        shadow-lg shadow-black/20
-        ${letter === 'enter' || letter === 'backspace' ? 'px-3' : 'px-2'}
+        ${letter === 'enter' || letter === 'backspace' ? 'px-2' : 'px-1'}
       `}
       onClick={handleClick}
     >
