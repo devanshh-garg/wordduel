df6a9d128becf8b9a1ae35444541c9b62a6d055d
devanshh-garg <devanshgarg.penn@gmail.com>
2025-05-27 03:49:22 +0530

    Deploy application to Netlify

--- src/components/KeyboardRow.tsx
+++ src/components/KeyboardRow.tsx
@@ -10,7 +10,7 @@ interface KeyboardRowProps {
 
 const KeyboardRow: React.FC<KeyboardRowProps> = ({ keys, keyStates, onKeyClick }) => {
   return (
-    <div className="flex justify-center gap-1.5 my-1.5">
+    <div className="flex justify-center gap-1.5 my-1">
       {keys.map((key, index) => {
         const lowerCaseKey = key.toLowerCase();
         const state = keyStates[lowerCaseKey] || 'unused';
