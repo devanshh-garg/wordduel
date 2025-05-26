516d374126a6e9df85fc6ebf018cce885d5e3fc4
devanshh-garg <devanshgarg.penn@gmail.com>
2025-05-27 03:31:39 +0530

    Updated Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="fixed bottom-2 right-4 text-xs text-slate-500 dark:text-slate-400">
+      created with love, by Devansh.
+    </footer>
+  );
+};
+
+export default Footer; 
